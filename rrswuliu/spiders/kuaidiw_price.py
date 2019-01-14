0fd6a34713e0c76464cc53a7c86e7b0bb453793d
SFSeiei <ps740969264@2980.com>
2019-01-14 14:23:52 +0800

    提交

--- rrswuliu/spiders/kuaidiw_price.py
+++ rrswuliu/spiders/kuaidiw_price.py
@@ -83,4 +83,4 @@ class KuaidiwPriceSpider(scrapy.Spider):
         kuaidiw_price爬虫结束！
         '''
         subject = u'爬虫结束邮件'
-        mailer.send(to=["740969264@qq.com"], subject = subject, body = body)
+        mailer.send(to=["******@qq.com"], subject = subject, body = body)
